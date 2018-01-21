ad1c0c02d95d1b141328bb172e7cfaddc93d1b9b
rjqiao <rjqiao@gmail.com>
2018-01-21 14:30:24 -0800

    launch script

---
+++ launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+fuser -k 3000/tcp
+
+# service redis_6379 start
+brew services start redis
+cd ./oj-server
+npm install
+nodemon server.js &
+cd ../oj-client
+npm install
+ng build --watch
+
+echo "=================================================="
+read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
+
+fuser -k 3000/tcp
+# service redis_6379 stop
+brew services stop redis
